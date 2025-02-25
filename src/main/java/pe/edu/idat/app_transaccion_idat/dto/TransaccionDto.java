62a7537e65ca6d4be4777ff0c65785d0a0a9610c
JosuePV12 <josuepuio18@gmail.com>
2025-02-24 20:49:54 -0500

    mapeo de la tabla transaccionDto

--- src/main/java/pe/edu/idat/app_transaccion_idat/dto/TransaccionDto.java
+++ src/main/java/pe/edu/idat/app_transaccion_idat/dto/TransaccionDto.java
@@ -1,4 +1,20 @@
 package pe.edu.idat.app_transaccion_idat.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Table(name = "transaccion")
+@Entity
 public class TransaccionDto {
+    private Integer id;
+    private Integer cuentaorigenid;
+    private Integer cuentadestinoid;
+    private Double monto;
+    private LocalDateTime fecha;
 }
